a23ae3ec241a0db74463c52cb3d837782fafc779
codemilli <hckrmoon@gmail.com>
2020-08-06 15:27:28 +0900

    add PreRegistrationSection

--- src/views/page-components/HomePage/HomePage.tsx
+++ src/views/page-components/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import css from './HomePage.module.scss';
 import Header from "@components/Header/Header";
 import HeroSection from "@components/HeroSection/HeroSection";
 import AboutSection from "@components/AboutSection/AboutSection";
+import PreRegistrationSection from "@components/PreRegistrationSection/PreRegistrationSection";
 
 interface HomePageProps {}
 
@@ -12,6 +13,7 @@ const HomePage: React.FC<HomePageProps> = () => {
       <Header/>
       <HeroSection/>
       <AboutSection/>
+      <PreRegistrationSection/>
     </div>
   );
 }
