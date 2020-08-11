3e087889dc8fa820e5af6499d0ca5ab592f18ae1
codemilli <hckrmoon@gmail.com>
2020-08-11 17:46:28 +0900

    apply mobile layout

--- src/views/components/NoticeSection/NoticeSection.tsx
+++ src/views/components/NoticeSection/NoticeSection.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import css from './NoticeSection.module.scss';
+import ArrowDownIcon from "@svgs/ArrowDownIcon/ArrowDownIcon";
 
 interface NoticeSectionProps {}
 
@@ -16,13 +17,17 @@ const NoticeSection: React.FC<NoticeSectionProps> = () => {
       <div className={css.socialPlatformContainer}>
         {[...Array(3).keys()].map((i) =>
           <div key={i} className={css.platform}>
-            <div className={css.icon}></div>
+            <div className={css.icon}/>
             <h3>프론트엔드개발그룹</h3>
+            <div className={css.mobileTitleWrap}>
+              <div className={css.icon}/>
+              <h3>프론트엔드개발그룹</h3>
+            </div>
             <p>
               프론트엔드개발그룹의 공식 페이스북 그룹
               입니다. 최신 공지사항을 확인할 수 있습니다.
             </p>
-            <a href=""> {'->'} </a>
+            <a href=""> <ArrowDownIcon/> </a>
           </div>
         )}
       </div>
--- src/views/components/RegisterSection/RegisterSection.tsx
+++ src/views/components/RegisterSection/RegisterSection.tsx
@@ -8,7 +8,7 @@ const RegisterSection: React.FC<RegisterSectionProps> = () => {
   return (
     <div className={css.RegisterSection}>
       <div className={css.container}>
-        <h2>REGISTER <span>Now</span></h2>
+        <h2>REGISTER</h2>
         <h4>지금, 바로 등록하세요</h4>
         <div className={css.buttonWrap}>
           <RegisterButton>사전 등록하기</RegisterButton>
