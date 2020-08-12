64cebacc23b492a01aa0449ec33cfd055390100d
codemilli <hckrmoon@gmail.com>
2020-08-12 14:20:11 +0900

    edit viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <meta charSet="UTF-8"/>
-        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
       </Head>
       <Component {...pageProps}/>
     </>
