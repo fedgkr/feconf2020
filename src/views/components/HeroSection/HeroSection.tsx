d935f5e05f4aaef3604195969f373a490cb24f33
daybrush <daybrush@gmail.com>
2020-08-18 19:02:53 +0900

    add gradient circle motion

--- src/views/components/HeroSection/HeroSection.tsx
+++ src/views/components/HeroSection/HeroSection.tsx
@@ -5,12 +5,43 @@ import ArrowDownIcon from "@svgs/ArrowDownIcon/ArrowDownIcon";
 import {motion} from "framer-motion";
 import {useIntersection} from "@utils/hooks/use-intersection";
 import heroMotions from "@motions/hero.motion";
+import Scene from 'scenejs';
 
 interface HeroSectionProps {}
 
 const HeroSection: React.FC<HeroSectionProps> = () => {
   const sectionRef = useRef();
   const { visible } = useIntersection(sectionRef);
+
+  React.useEffect(() => {
+    const scene = new Scene({
+      [`.${css.circleBackground}`]: {
+        0: {
+          "transform": "translate(0px, 0px) rotate(0deg)",
+          "transform-origin": "1800px 600px",
+        },
+        10: {
+          "transform": "translate(1200px, -1200px)",
+          "transform-origin": "600px 1800px",
+        },
+        "10>": {
+          "transform": "translate(0px, 0px) rotate(180deg)",
+          "transform-origin": "1800px 600px",
+        },
+        20: {
+          "transform": "translate(1200px, -1200px) rotate(360deg)",
+          "transform-origin": "600px 1800px",
+        },
+      },
+    }, {
+      selector: true,
+      iterationCount: "infinite",
+    }).playCSS();
+
+    return () => {
+      scene.clear();
+    };
+  }, []);
   return (
     <motion.div
       ref={sectionRef}
@@ -28,7 +59,9 @@ const HeroSection: React.FC<HeroSectionProps> = () => {
           </motion.p>
         </div>
       </div>
-      <motion.div className={css.circle} variants={heroMotions.circle}/>
+      <motion.div className={css.circle} variants={heroMotions.circle}>
+        <div className={css.circleBackground}></div>
+      </motion.div>
       <motion.div className={css.smallCircle} variants={heroMotions.smallCircle}/>
       <motion.div className={css.dashedCircle} variants={heroMotions.dashedCircle}/>
       <motion.div className={css.dashedSmallCircle} variants={heroMotions.circle}/>
