3c6d1f78de759f1c9291f096330ba6f18c436bea
JaeYeopHan <ljyhanll@gmail.com>
2020-08-25 16:49:53 +0900

    fix: change word

--- src/views/components/AboutSection/AboutSection.tsx
+++ src/views/components/AboutSection/AboutSection.tsx
@@ -41,7 +41,7 @@ const AboutSection: React.FC<AboutSectionProps> = () => {
             프론트엔드를 개발하며 마주했던 치열한 고민과 깊은 인사이트를 공유하며 여러분과 함께 성장하려고 합니다.
           </motion.p><br/>
           <motion.p variants={aboutMotions.aboutText}>
-            FEConf 공식 Facebook 페이지를 구독하시면 행사에 대한 정보를 보다 빠르게 받으실 수 있습니다. <SafeLink href="https://www.facebook.com/feconf.kr/">Facebook 페이지</SafeLink>의 공지 사항을 꼭 확인해주세요!
+            FEConf 공식 Facebook 페이지를 구독하시면 행사에 대한 정보를 보다 빠르게 받을 수 있습니다. <SafeLink href="https://www.facebook.com/feconf.kr/">Facebook 페이지</SafeLink>의 공지 사항을 꼭 확인해주세요!
           </motion.p>
         </motion.div>
       </motion.div>
