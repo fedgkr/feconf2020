b25fb67c25b42cf0956f78b806208e47f27e1c30
codemilli <hckrmoon@gmail.com>
2020-08-26 00:37:38 +0900

    prevent ios mobile input zoom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ function App({Component, pageProps}) {
     <>
       <Head>
         <meta charSet="UTF-8"/>
-        <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0"/>
         <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
         <title>FEConf 2020 at Home</title>
         <meta
