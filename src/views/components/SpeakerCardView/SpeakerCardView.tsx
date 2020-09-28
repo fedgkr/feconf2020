c199034aab92fee6ee5106c48d450d6a0812912e
codemilli <hckrmoon@gmail.com>
2020-09-28 17:46:15 +0900

    add parallel motion for sponsors

--- src/views/components/SpeakerCardView/SpeakerCardView.tsx
+++ src/views/components/SpeakerCardView/SpeakerCardView.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import css from './SpeakerCardView.module.scss';
 import {Speaker} from "@constants/types";
 import {motion} from "framer-motion";
-import speakerListMotions from "@motions/speakerList.motion";
 
 interface SpeakerCardViewProps {
   speaker: Speaker;
+  variants?: any;
 }
 
-const SpeakerCardView: React.FC<SpeakerCardViewProps> = ({ speaker }) => {
+const SpeakerCardView: React.FC<SpeakerCardViewProps> = ({ speaker, variants }) => {
   return (
-    <motion.div className={css.SpeakerCardView} variants={speakerListMotions.item}>
+    <motion.div className={css.SpeakerCardView} variants={variants}>
       <img src={`/images/speakers/${speaker.name}.png`} alt={speaker.name} draggable={false}/>
       <div className={css.info}>
         <h4 className={css.name}>{speaker.name}</h4>
