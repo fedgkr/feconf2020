bc56de7e4d0364898b9d3ddba4ab5d99e456ee07
codemilli <hckrmoon@gmail.com>
2020-09-30 05:38:41 +0900

    add gradient images

--- src/views/components/SessionDetailModal/SessionDetailModal.tsx
+++ src/views/components/SessionDetailModal/SessionDetailModal.tsx
@@ -1,18 +1,30 @@
 import React, {useCallback} from 'react';
 import css from './SessionDetailModal.module.scss';
-import {motion} from "framer-motion";
 import {useDispatch} from "react-redux";
 import ModalContainer from "@components/ModalContainer/ModalContainer";
 import {setSession} from "@store/slices/sessionSlice";
 import {useSessionState} from "@store/index";
+import {Session} from "@constants/types";
 
 interface SessionDetailModalProps {
   active: boolean;
 }
 
+const getOrder = (sessions: Session[], selectedSession: Session): number => {
+  return sessions.filter(s => s.track === selectedSession?.track).reduce<number>((order, session, idx) => {
+    if (order) {
+      return order;
+    }
+    if (session.title === selectedSession.title) {
+      return idx + 1;
+    }
+  }, null);
+}
+
 const SessionDetailModal: React.FC<SessionDetailModalProps> = ({ active }) => {
-  const { selectedSession } = useSessionState()
+  const { sessions, selectedSession } = useSessionState()
   const dispatch = useDispatch();
+  const order = getOrder(sessions, selectedSession);
   const onClose = useCallback(() => {
     dispatch(setSession(null));
   }, []);
@@ -20,13 +32,13 @@ const SessionDetailModal: React.FC<SessionDetailModalProps> = ({ active }) => {
     <ModalContainer active={active} maxWidth={700} onClose={onClose}>
       <div className={css.SessionDetailModal}>
         <div className={css.iconContainer}>
-          <div className={css.icon}></div>
+          <div className={css.icon} style={{ backgroundImage: `url("/images/backgrounds/img-gradient-${order}.png")` }}/>
         </div>
         <h3 className={css.title}>
           {selectedSession?.title || ' '}
         </h3>
         <div className={css.profile}>
-          <img src={`/images/speakers/${selectedSession?.speaker.name}.png`} alt={selectedSession?.speaker.name}/>
+          <div className={css.image} style={{ backgroundImage: `url(/images/speakers/${selectedSession?.speaker.name}.png)` }}/>
           <div className={css.textWrap}>
             <span>{selectedSession?.speaker.name}</span>
             <span>{selectedSession?.speaker.company + ' ' + selectedSession?.speaker.role}</span>
--- src/views/components/SpeakerCardView/SpeakerCardView.tsx
+++ src/views/components/SpeakerCardView/SpeakerCardView.tsx
@@ -1,8 +1,7 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
+import React, {CSSProperties, useCallback, useEffect, useRef, useState} from 'react';
 import css from './SpeakerCardView.module.scss';
 import {Speaker} from "@constants/types";
 import {motion} from "framer-motion";
-import classcat from "classcat";
 
 interface SpeakerCardViewProps {
   speaker: Speaker;
@@ -14,10 +13,16 @@ const defaultCardImageStyle = {
   opacity: 1,
 };
 
-const useTranslate = (order) => {
+const colorSet = [
+  'linear-gradient(209deg, #6cabfe 100%, #ec20c0 -2%), linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .3))',
+  'linear-gradient(209deg, #694ad9 100%, #a29cfe -2%), linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .3))',
+  'linear-gradient(209deg, #f37121 100%, #e91482 -2%), linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .3))',
+];
+
+const useTranslatingImage = (order: number, imageUrl: string) => {
   const wrapperEl = useRef<HTMLDivElement>();
   const firstEffect = useRef(true);
-  const [style, setStyle] = useState<React.CSSProperties>({});
+  const [style, setStyle] = useState<CSSProperties>({});
   const hasOrder = order !== undefined;
   const onIntersect = useCallback(([entry]) => {
     let opacity = 1, transform = 'translate3d(0, 0, 0) scale(1)';
@@ -26,7 +31,9 @@ const useTranslate = (order) => {
       transform = `translate3d(${x <= 0 ? -150 : 150}px, 0, 0) scale(.9)`;
       opacity = .6;
     }
-    const style = hasOrder ? { opacity, transform, transitionDelay: `${firstEffect.current ? order * 80 : 0}ms` } : defaultCardImageStyle;
+    const style:CSSProperties = hasOrder ? { opacity, transform, transitionDelay: `${firstEffect.current ? order * 80 : 0}ms` } : defaultCardImageStyle;
+    const blendColor = colorSet[order % 3];
+    style.backgroundImage = `${blendColor}, url(${imageUrl})`;
     setStyle(style);
   }, []);
   useEffect(() => {
@@ -37,25 +44,15 @@ const useTranslate = (order) => {
   useEffect(() => {
     const timeout = setTimeout(() => firstEffect.current = false, 500);
     return () => clearTimeout(timeout);
-  }, [])
+  }, []);
   return { wrapperEl, style };
 }
 
-const useBGColor = (order?) => {
-  if (order === undefined) {
-    return null;
-  }
-  return css[`color${order % 3}`];
-}
-
-const SpeakerCardView: React.FC<SpeakerCardViewProps> = ({ speaker, order, variants }) => {
-  const { wrapperEl, style } = useTranslate(order);
-  const colorClass = useBGColor(order);
+const SpeakerCardView: React.FC<SpeakerCardViewProps> = React.memo(({ speaker, order, variants }) => {
+  const { wrapperEl, style } = useTranslatingImage(order, `/images/speakers/${speaker.name}.png`);
   return (
     <motion.div ref={wrapperEl} className={css.SpeakerCardView} variants={variants}>
-      <div style={{ ...style, backgroundImage: `url(/images/speakers/${speaker.name}.png)` }} className={css.imageWrap}>
-        <div className={classcat([css.bg, colorClass])}/>
-      </div>
+      <div style={style} className={css.imageWrap}/>
       <div className={css.info}>
         <h4 className={css.name}>{speaker.name}</h4>
         <div className={css.role}>
@@ -65,6 +62,6 @@ const SpeakerCardView: React.FC<SpeakerCardViewProps> = ({ speaker, order, varia
       </div>
     </motion.div>
   );
-}
+});
 
 export default SpeakerCardView;
