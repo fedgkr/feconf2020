f958cf291e3eca919e1ce82040ceb9a7c35fb593
JaeYeopHan <ljyhanll@gmail.com>
2020-10-14 11:08:07 +0900

    fix: fix typo

--- src/resources/data.ts
+++ src/resources/data.ts
@@ -31,7 +31,7 @@ export const sessionList: Session[] = [
   },
   {
     title: 'Relay, 그리고 Declarative에 대해 다시 생각하기',
-    description: '본 발표에서는 당근마켓의 두번째 탭(내근처)을 웹으로 전환하게 된 계기와 내근처 탭을 구성하는 기술적 기반인 Relay와 GraphQL 클라이언트 스키마에 대해 소개합니다. 또한 Relay를 제품에 실제로 적용하면서 경험한 장점과 단점, 그리고 최종적으로 왜 Relay가 React의 마지막 퍼즐 조각인지 깨달은 바를 공유합니다.',
+    description: "당근마켓의 두번째 탭('내 근처')을 웹으로 전환하게 된 계기와 프로덕트를 구성하는 기술인 Relay와 GraphQL 클라이언트 스키마에 대해 소개합니다. 또한 Relay를 제품에 실제로 적용하면서 경험한 장점과 단점, 그리고 최종적으로 왜 Relay가 React의 마지막 퍼즐 조각인지 깨달은 바를 공유합니다.",
     track: Track.A,
     startTime: '14:40',
     endTime: '15:20',
@@ -45,8 +45,8 @@ export const sessionList: Session[] = [
   {
     title: '한글 파일로부터 우리를 구원해줄 hwp.js',
     description: '한글 파일을 읽고 렌더링하려는 여러번의 시도가 있었습니다.\n' +
-      'hwp.js는 크로스 플렛폼 리더, 뷰어를 구현하기위해 웹기술을 이용해 한글 파일을 읽고 렌더링하는 프로젝트 입니다. \n' +
-      'hwp.js를 만들며 배운 브라우저에서 바이너리 파일을 다루는법, 폰트와 절대단위 프린트 속성등을 공유하고자 합니다.',
+      'hwp.js는 크로스 플렛폼 리더, 뷰어를 구현하기 위해 웹 기술을 이용해 한글 파일을 읽고 렌더링하는 프로젝트 입니다. \n' +
+      'hwp.js를 만들며 배운 브라우저에서 바이너리 파일을 다루는법, 폰트와 절대 단위 프린트 속성등을 공유하고자 합니다.',
     track: Track.A,
     startTime: '15:30',
     endTime: '16:10',
@@ -58,7 +58,7 @@ export const sessionList: Session[] = [
   },
   {
     title: 'OpenAPI Specification으로 타입 세이프하게 API 개발하기 - 희망편 vs 절망편',
-    description: 'GraphQL 등 좋은 기술이 많이 나왔음에도 불구하고, 여전히 서버에서 RESTful API로만 작업되는 경우가 많습니다. 서버와 클라이언트가 API 명세를 효과적으로 공유할 수 있는 방법인 OpenAPI Specification을 알아보고, 이를 활용해 타입 세이프한 API 클라이언트를 만들고 개발 과정을 개선하는 방법을 실제 사례와 함께 알아봅니다.',
+    description: 'GraphQL과 같은 기술이 많이 나왔음에도 불구하고, 여전히 서버에서 RESTful API로만 작업되는 경우가 많습니다. 서버와 클라이언트가 API 명세를 효과적으로 공유할 수 있는 방법인 OpenAPI Specification을 알아보고, 이를 활용해 타입 세이프한 API 클라이언트를 만들고 개발 과정을 개선하는 방법을 실제 사례와 함께 알아봅니다.',
     track: Track.A,
     startTime: '16:30',
     endTime: '17:10',
@@ -84,7 +84,7 @@ export const sessionList: Session[] = [
   },
   {
     title: 'Statecharts: 복잡한 UI 상태를 지배하는 방법',
-    description: '데브시스터즈에서 다양한 웹 서비스를 개발하면서 다뤘던 크고 작은 상태, 이런 상태들을 효과적으로 관리하고 설명하기 위한 훌륭한 멘탈모델이 되어준 Harel statecharts에 대해서 소개합니다.',
+    description: '데브시스터즈에서 다양한 웹 서비스를 개발하면서 다뤘던 크고 작은 상태, 이런 상태들을 효과적으로 관리하고 설명하기 위한 훌륭한 멘탈 모델이 되어준 Harel statecharts에 대해서 소개합니다.',
     track: Track.A,
     startTime: '18:10',
     endTime: '18:50',
@@ -138,7 +138,7 @@ export const sessionList: Session[] = [
   {
     title: 'iframe을 활용하여 외부 Service를 통합하기',
     description: '프론트엔드 개발자로서 실제 운영되고 있는 B2B 서비스에 iframe integration을 구현하기 위한 노력을 담은 개발기입니다.\n'+
-    '다루기 조금 까다롭지만, 잘 이용하면 재밌는 기능을 만들 수 있는 iframe.\n'+
+    '다루기는 조금 까다롭지만, 잘 활용하면 재밌는 기능을 만들 수 있는 iframe.\n'+
     '제 발표를 통해 좀 더 친해져 보는 것은 어떨까요?',
     track: Track.B,
     startTime: '15:30',
@@ -152,7 +152,7 @@ export const sessionList: Session[] = [
   },
   {
     title: 'React Native에서 Pinch Zoom을 구현하면서 배운것들',
-    description: '핀치줌을 직접 구현한 경험을 바탕으로 발표시간 동안 처음부터 같이 구현해보는 시간을 가질 예정입니다. 여기서 구현한 방법이 최적의 방법이 아닐 수 있다고 생각합니다. 하지만 구현하는 과정을 통해서 ReactNative 의 panResponder 나 Animated 에 대한 이해가 깊어졌고 더 잘 사용할 수 있게 되었습니다. ',
+    description: '핀치줌을 직접 구현한 경험을 바탕으로 발표시간 동안 처음부터 같이 구현해보는 시간을 가집니다. 여기서 구현한 방법이 최적의 방법이 아닐 수 있다고 생각하지만 구현하는 과정을 통해서 ReactNative 의 panResponder 나 Animated 에 대한 이해가 깊어졌고 더 잘 사용할 수 있게 되었습니다. ',
     track: Track.B,
     startTime: '16:30',
     endTime: '17:10',
@@ -165,7 +165,7 @@ export const sessionList: Session[] = [
   },
   {
     title: 'Chrome DevTools Protocol을 활용하여 Debugger 개발하기',
-    description: 'Chrome DevTools Protocol을 활용하면 Chrome 개발자 도구를 확장할 수 있고, VS Code 디버거와 같은 커스텀 디버거를 개발할 수 있습니다. NAVER Lambda Serverless Platform의 디버거 개발 경험을 바탕으로 Protocol 기초 사용법과 사례를 공유하고 활용 인사이트를 제공하고자 합니다.',
+    description: 'Chrome DevTools Protocol을 활용하면 Chrome 개발자 도구를 확장할 수 있고, VSCode 디버거와 같은 커스텀 디버거를 개발할 수 있습니다. NAVER Lambda Serverless Platform의 디버거 개발 경험을 바탕으로 Protocol 기초 사용법과 사례를 공유하고 활용 인사이트를 제공하고자 합니다.',
     track: Track.B,
     startTime: '17:20',
     endTime: '18:00',
@@ -178,7 +178,7 @@ export const sessionList: Session[] = [
   },
   {
     title: '복잡한 백오피스에서 Form의 상태 다루기',
-    description: '최근에 물류 백오피스를 만들면서 많은 Input들을 다뤄야 했습니다. 작업을 진행하면서 Context API만으로 작업을 시작했고 퍼포먼스 문제에 부딪혔습니다. 해당 문제를 해결하기 위해서 어떤 시행착오를 겪고 어떻게 해결했는 지를 경험담을 공유하고자 합니다. 또한 스스로 무의식적으로 사용하던 상태관리 라이브러리의 최적화 방안에 대해서 소개합니다.',
+    description: '최근에 물류 백오피스를 만들면서 많은 Input들을 다뤄야 했습니다. 작업을 진행하면서 Context API만으로 작업을 시작했고 퍼포먼스 문제에 부딪혔습니다. 해당 문제를 해결하기 위해서 어떤 시행착오를 겪고 어떻게 해결했는 지를 공유하고자 합니다. 또한 스스로 무의식적으로 사용하던 상태 관리 라이브러리의 최적화 방안에 대해서 소개합니다.',
     track: Track.B,
     startTime: '18:10',
     endTime: '18:50',
