cad5b7be7a3c47d762a8fba845f636ccf25e3ca2
codemilli <hckrmoon@gmail.com>
2020-10-30 22:46:18 +0900

    fix bug

--- src/utils/hooks/use-liveState.ts
+++ src/utils/hooks/use-liveState.ts
@@ -4,13 +4,13 @@ import {useEffect, useState} from "react";
 export const useLiveState = (session: Session) => {
   const [isLive, setLive] = useState(false);
   useEffect(() => {
-    const timeout = setTimeout(() => {
+    const interval = setInterval(() => {
       const now = +new Date();
       const { startTimestamp, endTimestamp } = session;
       setLive(startTimestamp <= now && now <= endTimestamp);
     }, 1000);
     return () => {
-      clearTimeout(timeout);
+      clearTimeout(interval);
     };
   }, []);
   return isLive;
